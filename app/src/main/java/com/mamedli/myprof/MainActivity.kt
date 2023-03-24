ce9983f48d2dbbc2d196d75a624e6dcd5befb67a
emilm <emil.mamedlov7779@gmail.com>
2023-03-24 14:46:01 +0300

    Create proj, drawer Menu

---
+++ app/src/main/java/com/mamedli/myprof/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.mamedli.myprof
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import com.google.android.material.navigation.NavigationView
+import com.mamedli.myprof.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        init()
+    }
+
+    private fun init(){
+        var toggle = ActionBarDrawerToggle(
+            this, binding.drawerLayout, binding.mainContent.toolbar, R.string.open, R.string.close)
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        binding.navView.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.id_public -> {
+
+            }
+            R.id.id_chats -> {
+
+            }
+            R.id.id_settings -> {
+
+            }
+            R.id.id_career_guidance -> {
+
+            }
+            R.id.id_reference -> {
+
+            }
+            R.id.id_account -> {
+
+            }
+        }
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+}
