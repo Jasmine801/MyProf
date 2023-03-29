397bb707e92f4501ac4f47072a34ade4e537f0fc
emilm <emil.mamedlov7779@gmail.com>
2023-03-29 19:04:07 +0300

    Fragment Navigate

--- app/src/main/java/com/mamedli/myprof/fragments/PublicationsFragment.kt
+++ app/src/main/java/com/mamedli/myprof/fragments/PublicationsFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.mamedli.myprof.R
 import com.mamedli.myprof.databinding.FragmentPublicationsBinding
 
@@ -13,9 +14,9 @@ class PublicationsFragment : Fragment() {
 
     lateinit var binding: FragmentPublicationsBinding
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(
@@ -23,11 +24,20 @@ class PublicationsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentPublicationsBinding.inflate(inflater, container, false)
+        onClickNewPublication()
         return binding.root
+
+    }
+
+    private fun onClickNewPublication(){
+        val controller = findNavController()
+        binding.ibAddPublication.setOnClickListener {
+            controller.navigate(R.id.newPublicationFragment)
+        }
     }
 
     companion object {
         @JvmStatic
-        fun newInstance(param1: String, param2: String) = PublicationsFragment()
+        fun newInstance() = PublicationsFragment()
     }
 }
