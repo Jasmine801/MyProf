c2a2f6270e906abef79df08f8f9f6338d72204b9
emilm <emil.mamedlov7779@gmail.com>
2023-05-07 17:24:25 +0300

    authorization firebase

--- app/src/main/java/com/mamedli/myprof/activities/AuthorizationActivity.kt
+++ app/src/main/java/com/mamedli/myprof/activities/AuthorizationActivity.kt
@@ -1,12 +1,74 @@
 package com.mamedli.myprof.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.mamedli.myprof.R
+import com.mamedli.myprof.databinding.ActivityAuthorizationBinding
 
 class AuthorizationActivity : AppCompatActivity() {
+
+    lateinit var launcher: ActivityResultLauncher<Intent>
+    lateinit var auth: FirebaseAuth
+    lateinit var binding: ActivityAuthorizationBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_authorization)
+        binding = ActivityAuthorizationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        auth = Firebase.auth
+        launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(it.data)
+
+            try{
+                val account = task.getResult(ApiException::class.java)
+                if(account != null){
+                    firebaseAuthWithGoogle(account.idToken!!)
+                }
+            }
+            catch (e: ApiException){
+                Log.d("login", "ApiException")
+            }
+        }
+
+        binding.bLogIn.setOnClickListener { signInWithGoogle() }
+    }
+
+    private fun getClient() : GoogleSignInClient{
+        val gso = GoogleSignInOptions
+            .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        return GoogleSignIn.getClient(this, gso)
+    }
+
+    private fun signInWithGoogle(){
+        val signInClient = getClient()
+        launcher.launch(signInClient.signInIntent)
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String){
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential).addOnCompleteListener{
+            if(it.isSuccessful){
+                Log.d("login", "Google sign in done")
+            }else{
+                Log.d("login", "Error Google sign in")
+            }
+        }
     }
 }
